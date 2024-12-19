2d1b4f436ef31b91763644d10f27e102d3a2d136
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2024-12-19 19:24:25 -0300

    feat: create server

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.listen(3000, () => {
+  console.log("Server listening!");
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).send("Rota iniciada!");
+});
